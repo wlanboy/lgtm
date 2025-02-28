41a8733771424ed2054f5805dffb2d6241ddb3ac
wlanboy <wlanboy@github.com>
2025-02-28 13:14:22 +0100

    init repro

---
+++ shared/start.sh
@@ -0,0 +1,3 @@
+docker run --name loki -d -v $(pwd):/mnt/config -p 3100:3100 grafana/loki:3.4.1 -config.file=/mnt/config/loki-config.yaml
+docker run --name promtail -d -v $(pwd):/mnt/config -v /var/log:/var/log --link loki grafana/promtail:3.4.1 -config.file=/mnt/config/promtail-config.yaml
+
